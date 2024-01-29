7d1587738d023fb81977ca639cc2e364dca41ee0
Shasatya <120493084+Shasatya@users.noreply.github.com>
2024-01-30 03:50:25 +0530

    added about

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.scss";
 import Home from "./components/Home/Home";
 import Contact from "./components/Contact/Contact";
+import About from "./components/About/About";
 
 function App() {
   return (
     <>
       <section>
         <Home />
+      </section>
+      <section>
+        <About />
       </section>
       <section>
         <Contact />
---
+++ src/components/About/About.jsx
@@ -0,0 +1,96 @@
+import "./About.scss";
+
+const About = () => {
+  return (
+    <>
+      <div className="about">
+        <h1>About Us</h1>
+
+        <p>
+          Welcome to Sparkling Stars, At Sparkling Stars, we believe in creating
+          a foundation that goes beyond academics. Our holistic approach focuses
+          on cultivating not only bright minds but also compassionate hearts and
+          confident spirits. Get, set, ready to dive into the place where
+        </p>
+        <p className="quote">
+          <q>
+            Excellence is a Tradition, Brilliance is the Standard and Every Star
+            Shines with Incredible Radiance.
+          </q>
+        </p>
+        <h2>Our Vision</h2>
+        <p>
+          To promote the creative potential of children through integration of
+          Performing Arts. To improve self confidence and creativty through
+          participation and involvement in co-curricular. To provide proper
+          opportunities for the physical and intellectual development of
+          students.
+        </p>
+        <h2>Our Mission</h2>
+        <p>
+          At sparkling stars learning focuses on holistic development of your
+          child. We believe that teaching children the skill to learn is the
+          best gift.
+        </p>
+        <h2>About the Director</h2>
+        <p>~ A visionary leader shaping success in every frame.</p>
+        <p className="about-director">
+          Mrs. Anita Soni, Director, Sparkling Stars School is an exemplary
+          education visionary with a rich background in teaching and an
+          exceptional flair for diverse activities. She is a dedicated,
+          innovative and an accomplished educationist. She is a beacon of
+          excellence and inspiration in the realm of education. With over twenty
+          decades of pedagogical experience, she has gracefully transitioned
+          from her roots as a Pre-primary teacher to an eminent personality.
+        </p>
+        <p className="about-director">
+          She has wonderful Educational Attainment and Expertise Armed with M.A.
+          in Sociology, B.Ed. and a wealth of teaching certifications. She has
+          attained Honours Diploma in Systems Management from NIT. She stands as
+          an embodiment of academic prowess. A diploma in Yoga further
+          underscores her holistic approach to nurturing young minds. Her
+          artistic talents shine brightly through her proficiency in art and
+          craft that she seamlessly integrates into the curriculum, fostering
+          creativity and fine motor skills among her students. Her versatility
+          extends to crafting captivating webinars and presentations, where she
+          eloquently imparts her wisdom to fellow educators.
+        </p>
+        <p className="about-director">
+          A seasoned Judo enthusiast and advocate of sports in education, she
+          ensures that physical well-being harmonises with intellectual growth.
+          Her profound belief in the transformative power of education led her
+          to secure the prestigious award of honour for excellence in the
+          National Education Policy (NEP) of 2020. She has consistently attended
+          seminars on topics ranging from
+          <q>Theatre and Drama Role in Education</q>
+          to <q>Experiential Learning Techniques</q>. She also participated in
+          the CENTA webinar series, exemplifying her commitment in staying
+          attuned to emerging educational trends. She has attended a workshop on
+          <q>National Educational Policy and its implications for teachers.</q>
+        </p>
+        <p className="about-director">
+          She stands as an oasis of educational excellence. With her impeccable
+          academic qualifications, multifaceted talents, and indomitable spirit,
+          she continues to inspire generations and redefine the very essence of
+          holistic education.
+        </p>
+        <p className="quote">
+          <q>
+            At Sparkling Stars School, we strive for excellence not as a
+            destination, but as a continuous journey guided by passion,
+            dedication, and the unwavering commitment to nurturing young minds.
+            Welcome to Sparkling Stars, a place where education is an art,
+            teaching is a passion, and every child is a shining star.
+          </q>
+        </p>
+        <p>
+          Mrs. Anita Soni <br />
+          Director <br />
+          Sparkling Stars School
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
